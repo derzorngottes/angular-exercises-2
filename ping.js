2a808a3b8c449a7b744ea4b78ac01b2fde300745
derzorngottes <amy.conchie@gmail.com>
2016-05-05 00:59:54 -0700

    Add server text on player divs

--- ping.js
+++ ping.js
@@ -2,6 +2,9 @@ var app = angular.module('myApp', []);
 
 app.controller('myCtrl', ['$scope', function myCtrl($scope) {
   $scope.vm = {};
+  $scope.vm.game = {
+    pointStreak: 0
+  }
   $scope.vm.player1 = {
     score: 0,
     serving: true,
@@ -13,7 +16,11 @@ app.controller('myCtrl', ['$scope', function myCtrl($scope) {
     gamesWon: 0
   };
 
-  $scope.increaseScore = function (player) {
+  $scope.increaseScore = function(player) {
     $scope.vm[player].score += 1;
+  }
+  $scope.resetGame = function() {
+    $scope.vm.player1.score = 0;
+    $scope.vm.player2.score = 0;
   }
 }]);
