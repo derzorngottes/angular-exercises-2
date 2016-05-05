21a79ba53900930dcd6e30145cf133716f4a2614
derzorngottes <amy.conchie@gmail.com>
2016-05-05 00:30:23 -0700

    Add properties to players

--- ping.js
+++ ping.js
@@ -2,9 +2,17 @@ var app = angular.module('myApp', []);
 
 app.controller('myCtrl', ['$scope', function myCtrl($scope) {
   $scope.vm = {};
-  $scope.vm.player1 = {};
-  $scope.vm.player2 = {};
-  $scope.reverseWord = function () {
-    $scope.vm.reversedWord = $scope.vm.word.split('').reverse().join('');
+  $scope.vm.player1 = {
+    score: 0,
+    serving: true,
+    gamesWon: 0
+  };
+  $scope.vm.player2 = {
+    score: 0,
+    serving: false,
+    gamesWon: 0
+  };
+  $scope.increaseScore = function () {
+    $scope.vm.[playerX].score += 1;
   }
 }]);
